6dd9c23b8bdb88332996ef82b8e7c3314722cbf8
sajid1545 <clashking1545@gmail.com>
2023-11-29 20:39:13 +0600

    module 13-1 done

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -10,13 +10,31 @@ const findLastStudentId = async () => {
         .lean();
 
     // 203003  0001
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // year, semesterCode, 4 digit number
 export const generateStudentId = async (payload: TAcademicSemester) => {
     // first time 0000
-    const currentId = (await findLastStudentId()) || (0).toString();
+    let currentId = (0).toString(); // 0000 by default
+
+    const lastStudentId = await findLastStudentId(); // jodi last student thake tahole last semester code ar year ta pete chai
+
+    //~ last id example  2030 01 0001
+    const lastStudentSemesterCode = lastStudentId?.substring(4, 6); // 01
+    const lastStudentYear = lastStudentId?.substring(0, 4); // 2030
+
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    if (
+        lastStudentId &&
+        lastStudentSemesterCode === currentSemesterCode &&
+        lastStudentYear === currentYear
+    ) {
+        currentId = lastStudentId.substring(6); // 0001
+    }
+
     let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
     incrementId = `${payload.year}${payload.code}${incrementId}`;
