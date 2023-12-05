41dec2a3dcad1c93640512b9521a08eac9bebb9b
Sajjad Abdullah <clashking1545@gmail.com>
2023-12-05 23:12:41 +0600

    15-8 completed

--- src/app/modules/Course/course.interface.ts
+++ src/app/modules/Course/course.interface.ts
@@ -10,6 +10,6 @@ export type TCourse = {
     prefix: string;
     code: number;
     credits: number;
-    preRequisiteCourses: string[];
+    preRequisiteCourses: [TPreRequisiteCourses];
     isDeleted?: boolean;
 };
--- src/app/modules/Course/course.services.ts
+++ src/app/modules/Course/course.services.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import QueryBuilder from '../../builder/QueryBuilder';
 import { CourseSearchableFields } from './course.constants';
 import { TCourse } from './course.interface';
@@ -41,10 +43,56 @@ const deleteCourseFromDB = async (id: string) => {
     return result;
 };
 
-const updateCourseIntoDB = async (id: string, payload: TCourse) => {
-    const result = await Course.findByIdAndUpdate(id, payload, {
-        new: true,
-    });
+const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
+    const { preRequisiteCourses, ...courseRemainingData } = payload;
+
+    // step 1 : Basic course info update
+    const updateBasicCourseInfo = await Course.findByIdAndUpdate(
+        id,
+        courseRemainingData,
+        {
+            new: true,
+            runValidators: true,
+        },
+    );
+
+    // step 2 : preRequisiteCourses update
+    // check if there is any pre requisite courses to update
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+        // filter out the delete field
+
+        // filter ==> console.log(deletePreRequisites); [ { course: '656f43096796915b9c89587f', isDeleted: true } ]
+        const deletePreRequisites = preRequisiteCourses
+            .filter((elem) => elem.course && elem.isDeleted)
+            .map((el) => el.course);
+
+        // map ==> console.log(deletePreRequisites);  ['656f43096796915b9c89587f'] j gula isDeleted true oigular course property dibe
+
+        const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
+            $pull: {
+                preRequisiteCourses: {
+                    course: { $in: deletePreRequisites },
+                },
+            },
+        });
+
+        // filter out the new course fields
+        const newPreRequisites = preRequisiteCourses.filter(
+            (elem) => elem.course && !elem.isDeleted,
+        );
+
+        // console.log(newPreRequisites); [ { course: '656f43096796915b9c89587f', isDeleted: false } ]
+
+        const newPreRequisitesCourses = await Course.findByIdAndUpdate(id, {
+            $addToSet: {
+                preRequisiteCourses: { $each: newPreRequisites },
+            },
+        });
+    }
+
+    const result = await Course.findById(id).populate(
+        'preRequisiteCourses.course',
+    );
     return result;
 };
 
