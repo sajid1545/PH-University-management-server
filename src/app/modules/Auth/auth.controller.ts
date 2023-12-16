3e23c1d3b332d25a6b85ab27e6bfc8d197f1d54a
Sajjad Abdullah <clashking1545@gmail.com>
2023-12-17 00:00:13 +0600

    18-9 done

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -15,8 +15,6 @@ const loginUser = catchAsync(async (req, res) => {
 });
 
 const changePassword = catchAsync(async (req, res) => {
-    console.log(req.user, req.body);
-
     const { ...passwordData } = req.body;
 
     const result = await AuthServices.changePassword(req.user, passwordData);
